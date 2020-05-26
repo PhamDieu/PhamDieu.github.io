57739cc8469e55e3546a18eac70634cd6fcaec2c
PhamDieu <64083418+PhamDieu@users.noreply.github.com>
2020-05-26 13:18:23 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Document</title>
+    <?php require('common/header.php')?>
+</head>
+<body>
+    <?php require('common/header.php')?>
+    <section id="homepage">
+
+    </section>
+    <?php require('common/footer.php')?>
+</body>
+</html>
